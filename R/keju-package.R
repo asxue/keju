0f450383ba97718f214583a42ba9007a698d8b23
Albert Xue <albertsxue@gmail.com>
2026-01-12 14:35:14 -0800

    added py_require to imported functions

--- R/keju-package.R
+++ R/keju-package.R
@@ -6,6 +6,7 @@
 #' @importFrom dplyr %>%
 #' @importFrom dplyr group_by
 #' @importFrom dplyr summarize
+#' @importFrom reticulate py_require
 #' @importFrom reticulate source_python
 #' @importFrom utils write.csv
 ## usethis namespace: end
